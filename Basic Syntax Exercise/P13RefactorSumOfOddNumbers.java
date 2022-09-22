a8d2164fd549903a345599aedceac9b3442f314e
JorjAngelov <kickboxer1913@gmail.com>
2022-09-22 11:10:30 +0300

    Add tasks for Exercise
    Problem Solutions for Exercise: Basic Syntax

---
+++ Basic Syntax Exercise/P13RefactorSumOfOddNumbers.java
@@ -0,0 +1,23 @@
+package BasicSyntax;
+
+import java.util.Scanner;
+
+public class P13RefactorSumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.println(2 * i + 1);
+            sum += 2 * i;
+            sum++;
+        }
+        System.out.printf("Sum: %d%n", sum);
+
+
+
+
+    }
+}
